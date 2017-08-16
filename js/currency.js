22e9ac0307c01d4f79405f1b5057ee817fccd1c0
AdamRS <rs.adam.k@gmail.com>
2017-08-16 09:19:53 +0200

    dodanie przewijanego paska z walutami

---
+++ js/currency.js
@@ -0,0 +1,164 @@
+var currency =[
+    {
+        "kod_waluty":"AUD",
+        "kraj":"Australia Kiribati Nauru Tuvalu Wyspa Bożego Narodzenia Wyspa Norfolk Wyspy Kokosowe",
+        "nazwa_waluty":"dolar australijski"
+    },
+    {
+        "kod_waluty":"BGN",
+        "kraj":"Bułgaria",
+        "nazwa_waluty":"lew"
+    },
+    {
+        "kod_waluty":"BRL",
+        "kraj":"Brazylia",
+        "nazwa_waluty":"real"
+    },
+    {
+        "kod_waluty":"CAD",
+        "kraj":"Kanada",
+        "nazwa_waluty":"dolar kanadyjski"
+    },
+    {
+        "kod_waluty":"CHF",
+        "kraj":"Liechtenstein Szwajcaria",
+        "nazwa_waluty":"frank szwajcarski"
+    },
+    {
+        "kod_waluty":"CNY",
+        "kraj":"Chiny",
+        "nazwa_waluty":"yuan renminbi"
+    },
+    {
+        "kod_waluty":"CZK",
+        "kraj":"Republika Czeska",
+        "nazwa_waluty":"korona czeska"
+    },
+    {
+        "kod_waluty":"DKK",
+        "kraj":	"Dania Grenlandia Wyspy Owcz",
+        "nazwa_waluty":"korona duńska"
+    },
+    {
+        "kod_waluty":"GBP",
+        "kraj":"Wielka Brytania",
+        "nazwa_waluty":"funt szterling"
+    },
+    {
+        "kod_waluty":"HKD",
+        "kraj":"Hongkong",
+        "nazwa_waluty":"dolar Hongkongu"
+    },
+    {
+        "kod_waluty":"HRK",
+        "kraj":"Chorwacja",
+        "nazwa_waluty":"kuna"
+    },
+    {
+        "kod_waluty":"HUF",
+        "kraj":"Węgry",
+        "nazwa_waluty":"forint"
+    },
+    {
+        "kod_waluty":"IDR",
+        "kraj":"Indonezja",
+        "nazwa_waluty":"rupia indonezyjska"
+    },
+    {
+        "kod_waluty":"ILS"
+        ,"kraj":"Izrael",
+        "nazwa_waluty":"szekel"
+    },
+    {
+        "kod_waluty":"INR",
+        "kraj":"Indie",
+        "nazwa_waluty":"rupia indyjska"
+    },
+    {
+        "kod_waluty":"JPY",
+        "kraj":"Japonia	",
+        "nazwa_waluty":"jen"
+    },
+    {
+        "kod_waluty":"KRW",
+        "kraj":"Korea Południowa",
+        "nazwa_waluty":"won południowokoreański"
+    },
+    {
+        "kod_waluty":"MXN",
+        "kraj":"Meksyk",
+        "nazwa_waluty":"peso meksykańskie"
+    },
+    {
+        "kod_waluty":"MYR",
+        "kraj":"Malezja",
+        "nazwa_waluty":"ringgit"
+    },
+    {
+        "kod_waluty":"NOK",
+        "kraj":"Norwegia",
+        "nazwa_waluty":"korona norweska"
+    },
+    {
+        "kod_waluty":"NZD",
+        "kraj":	"Niue Nowa Zelandia Pitcairn Tokelau Wyspy Cooka",
+        "nazwa_waluty":"dolar nowozelandzki"
+    },
+    {
+        "kod_waluty":"PHP",
+        "kraj":"Filipiny",
+        "nazwa_waluty":"peso filipińskie"
+    },
+    {
+        "kod_waluty":"PLN",
+        "kraj":"Polska",
+        "nazwa_waluty":"złoty"
+    },
+    {
+        "kod_waluty":"RON",
+        "kraj":"Rumunia",
+        "nazwa_waluty":"lej rumuński"
+    },
+    {
+        "kod_waluty":"RUB",
+        "kraj":"Rosja",
+        "nazwa_waluty":"rubel rosyjski"
+    },
+    {
+        "kod_waluty":"SEK",
+        "kraj":"Szwecja",
+        "nazwa_waluty":"korona szwedzka"
+    },
+    {
+        "kod_waluty":"SGD",
+        "kraj":"Singapur",
+        "nazwa_waluty":"dolar singapurski"
+    },
+    {
+        "kod_waluty":"THB",
+        "kraj":"Tajlandia",
+        "nazwa_waluty":"bat"
+    },
+    {
+        "kod_waluty":"TRY",
+        "kraj":"Turcja",
+        "nazwa_waluty":"lira turecka"
+    },
+    {
+        "kod_waluty":"USD",
+        "kraj":"Stany Zjednoczone",
+        "nazwa_waluty":"dolar amerykański"
+    },
+    {
+        "kod_waluty":"ZAR",
+        "kraj":"Republika Południowej Afryki",
+        "nazwa_waluty":"rand"
+    },
+    {
+        "kod_waluty":"EUR",
+        "kraj":	"Andora Austria Belgia Cypr Czarnogóra Estonia Finlandia Francja Francuskie  Terytoria Południowe i Antarktyczne Grecja Gujana Francuska Gwadelupa Hiszpania Irlandia Litwa Luksemburg Łotwa Majotta Malta Martynika Monako Niderlandy Niemcy  Portugalia Reunion Saint-Barthélemy Saint-Martin Saint-Pierre i Miquelon San Marino Słowacja Słowenia Stolica Apostolska; Państwo Watykańskie Włochy  Wyspy Alandzkie",
+        "nazwa_waluty":"euro"
+    },
+
+]
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var rates = null;
+var baseCurrency = null;
 
 
 var apiConnect = new XMLHttpRequest();
@@ -8,15 +9,48 @@ apiConnect.open("GET", "http://api.fixer.io/latest?base=EUR");
 apiConnect.onreadystatechange = function() {
     
     if(apiConnect.readyState === 4 && apiConnect.status ===200){
+        baseCurrency = JSON.parse(this.response).base;
         rates = JSON.parse(this.response).rates;
+        
+        //wczytanie listy walut do menu
         var navi = document.getElementById('navi');
         var naviList = '';
-
         Object.keys(rates).forEach(function(element) {
-            naviList += `<li>${element}</li>`
+            currency.forEach(function(element2){
+                if (element2.kod_waluty == element){
+                    naviList += `<li>${element2.nazwa_waluty} (${element})  </li>`;
+                }
+            },this);
+            
+                        
         }, this);
         navi.innerHTML= naviList;
+
+
+        //wczytanie walut do przewijanego paska z kursami
+        var animationText = document.getElementById('animationText');
+        var pText = `<p>`;
+        Object.keys(rates).forEach(function(element){
+            console.log(element);
+            pText+= `${element}: ${rates[element]}  `;
+        },this);
+        pText+= '</p>'
+        animationText.innerHTML = pText;
+
+
+        // wczytanie waluty bazowej 
+        baseCurrency = JSON.parse(this.response).base;
+        var baseCurrencyText = document.getElementById('baseCurrency');
+        pText = `<p>${baseCurrency}:</p>`;
+        baseCurrencyText.innerHTML = pText;
+        
+
+
     }
 };
 apiConnect.send();
+
+
+
+
 
